2ef3dd124241bc969b3c594a6a88f0ac7dcf8211
Tomasz <tomasz.dykty@student.wat.edu.pl>
2021-01-11 18:49:02 +0100

    zadanie 3,3,1 texst statyczny

--- Zadanie-3-3/src/main.cpp
+++ Zadanie-3-3/src/main.cpp
@@ -1,7 +1,17 @@
+//3,3,1
 #include <Arduino.h>
+#include <LiquidCrystal.h>
 
-void setup() {  
-}
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);//RS,E,DB4,DB5,DB6,DB7
 
-void loop() {  
+void setup() {
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);//wtyświetla w wierszu 1 na góże
+  lcd.print("Dykty");
 }
+void loop() {
+  lcd.setCursor(6, 0);
+  lcd.print(millis()/1000);
+  delay(500);
+}
+//przy wyjąciu poza zakres nie jest wyświetlane
